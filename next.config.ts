ee62b957f59497e3d6e39cbba7c6709d086baf74
이안 <99740275+SeojinSon@users.noreply.github.com>
2026-04-15 18:25:30 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *;",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
